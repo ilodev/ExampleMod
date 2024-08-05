fa9ba6817dbeaafff6be9e71b67a306b9adfbee7
jixxed <jixxed@jixxed.nl>
2024-08-05 18:20:23 +0200

    convert to template

---
+++ init.bat
@@ -0,0 +1,90 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+set "modname=##MOD_NAME##"
+set "namespace=##MOD_NAMESPACE##"
+
+echo Enter a Mod name and namespace. use the format ExampleMod for the name and Example.Mod for the namespace. 
+echo Do not use spaces, numbers or special characters. The project might load be able to load otherwise.
+echo Enter the mod name(ex: ExampleMod):
+set /p "replacename="
+echo Enter the mod namespace(ex: Example.Mod):
+set /p "replacenamespace="
+move /y "%cd%\Assets\Scenes\ModNameScene.unity" "%cd%\Assets\Scenes\%replacename%Scene.unity"
+
+set "input_file=%cd%\Assets\Scripts\ModName.asmdef.tmpl"
+set "output_file=%cd%\Assets\Scripts\%replacename%.asmdef"
+
+if not exist "%input_file%" (
+    echo File "%input_file%" not found!
+    exit /b 1
+)
+
+(for /f "usebackq delims=" %%i in ("%input_file%") do (
+    set "line=%%i"
+    set "line=!line:%modname%=%replacename%!"
+    set "line=!line:%namespace%=%replacenamespace%!"
+    echo !line!
+)) > "%output_file%"
+
+del "%input_file%"
+
+
+
+set "input_file=%cd%\Assets\Scripts\ModName.cs.tmpl"
+set "output_file=%cd%\Assets\Scripts\%replacename%.cs"
+
+if not exist "%input_file%" (
+    echo File "%input_file%" not found!
+    exit /b 1
+)
+
+(for /f "usebackq delims=" %%i in ("%input_file%") do (
+    set "line=%%i"
+    set "line=!line:%modname%=%replacename%!"
+    set "line=!line:%namespace%=%replacenamespace%!"
+    echo !line!
+)) > "%output_file%"
+
+del "%input_file%"
+
+
+set "input_file=%cd%\Assets\Scripts\patches\PrefabPatch.cs.tmpl"
+set "output_file=%cd%\Assets\Scripts\patches\PrefabPatch.cs"
+
+if not exist "%input_file%" (
+    echo File "%input_file%" not found!
+    exit /b 1
+)
+
+(for /f "usebackq delims=" %%i in ("%input_file%") do (
+    set "line=%%i"
+    set "line=!line:%modname%=%replacename%!"
+    set "line=!line:%namespace%=%replacenamespace%!"
+    echo !line!
+)) > "%output_file%"
+
+del "%input_file%"
+
+
+set "input_file=%cd%\Assets\About\About.xml.tmpl"
+set "output_file=%cd%\Assets\About\About.xml"
+
+if not exist "%input_file%" (
+    echo File "%input_file%" not found!
+    exit /b 1
+)
+
+(for /f "usebackq delims=" %%i in ("%input_file%") do (
+    set "line=%%i"
+    set "line=!line:%modname%=%replacename%!"
+    set "line=!line:%namespace%=%replacenamespace%!"
+    echo !line!
+)) > "%output_file%"
+
+del "%input_file%"
+
+echo Initialization complete!
+del init.bat
+del init.sh
+endlocal
---
+++ init.sh
@@ -0,0 +1,79 @@
+﻿#!/bin/bash
+
+modname="##MOD_NAME##"
+namespace="##MOD_NAMESPACE##"
+
+echo "Enter a Mod name and namespace. Use the format ExampleMod for the name and Example.Mod for the namespace."
+echo "Do not use spaces, numbers, or special characters. The project might not be able to load otherwise."
+read -p "Enter the mod name (ex: ExampleMod): " replacename
+read -p "Enter the mod namespace (ex: Example.Mod): " replacenamespace
+
+mv "Assets/Scenes/ModNameScene.unity" "Assets/Scenes/${replacename}Scene.unity"
+
+input_file="Assets/Scripts/ModName.asmdef.tmpl"
+output_file="Assets/Scripts/${replacename}.asmdef"
+
+if [ ! -f "$input_file" ]; then
+    echo "File $input_file not found!"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    line="${line//$modname/$replacename}"
+    line="${line//$namespace/$replacenamespace}"
+    echo "$line"
+done < "$input_file" > "$output_file"
+
+rm "$input_file"
+
+input_file="Assets/Scripts/ModName.cs.tmpl"
+output_file="Assets/Scripts/${replacename}.cs"
+
+if [ ! -f "$input_file" ]; then
+    echo "File $input_file not found!"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    line="${line//$modname/$replacename}"
+    line="${line//$namespace/$replacenamespace}"
+    echo "$line"
+done < "$input_file" > "$output_file"
+
+rm "$input_file"
+
+input_file="Assets/Scripts/patches/PrefabPatch.cs.tmpl"
+output_file="Assets/Scripts/patches/PrefabPatch.cs"
+
+if [ ! -f "$input_file" ]; then
+    echo "File $input_file not found!"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    line="${line//$modname/$replacename}"
+    line="${line//$namespace/$replacenamespace}"
+    echo "$line"
+done < "$input_file" > "$output_file"
+
+rm "$input_file"
+
+input_file="Assets/About/About.xml.tmpl"
+output_file="Assets/About/About.xml"
+
+if [ ! -f "$input_file" ]; then
+    echo "File $input_file not found!"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    line="${line//$modname/$replacename}"
+    line="${line//$namespace/$replacenamespace}"
+    echo "$line"
+done < "$input_file" > "$output_file"
+
+rm "$input_file"
+rm "init.bat"
+
+echo "Initialization complete!"
+rm "$0"
